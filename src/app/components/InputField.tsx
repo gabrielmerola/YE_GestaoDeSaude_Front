80dbab05299b3986104fca747829ffa152deed9f
jotaapee <galhardojoaopedro@gmail.com>
2024-03-05 21:09:05 -0300

    create some components

--- src/app/components/InputField.tsx
+++ src/app/components/InputField.tsx
@@ -1,59 +1,29 @@
-import {
-    VStack,
-    Image,
-    Text,
-    Box,
-    FormControl,
-    Input,
-    Button,
-    Link
-} from "native-base";
-import { TouchableOpacity } from "react-native";
+import { Input, FormControl } from "native-base";
 
-export default function Login() {
-    return (
-        <VStack flex={1} justifyContent={"center"} alignItems={"center"} p={5}>
-            <Box>
-                <FormControl mt={3}>
-                    <FormControl.Label>Email</FormControl.Label>
-                    <Input
-                        placeholder={"Insira seu endereço de e-mail"}
-                        size={"lg"}
-                        w={"100%"}
-                        borderRadius={"lg"}
-                        backgroundColor={"gray.100"}
-                        shadow={3}
-                    />
-                </FormControl>
-                <FormControl mt={3}>
-                    <FormControl.Label>Senha</FormControl.Label>
-                    <Input
-                        placeholder={"Insira seu endereço de senha"}
-                        size={"lg"}
-                        w={"100%"}
-                        borderRadius={"lg"}
-                        backgroundColor={"gray.100"}
-                        shadow={3}
-                    />
-                </FormControl>
-            </Box>
-            <Button
-                w={"100%"}
-                backgroundColor={"blue.800"}
-                mt={10}
-                borderRadius={"lg"}
-            >
-                Entrar
-            </Button>
-            <Link href={"https://www.google.com"} mt={2}>
-                Esqueceu sua senha?
-            </Link>
-            <Box w={"100%"} flexDirection={"row"} justifyContent={"center"} mt={8}>
-                <Text>Ainda não tem cadastro? </Text>
-                <TouchableOpacity>
-                    <Text color={"blue.500"}>Faça seu cadastro!</Text>
-                </TouchableOpacity>
-            </Box>
-        </VStack>
-    );
+interface InputProps {
+  label?: string;
+  placeholder: string;
+  secureTextEntry?: boolean;
+  leftIcon?: React.ReactNode;
 }
+
+export function InputField ({ 
+  label, 
+  placeholder, 
+  secureTextEntry = false
+} : InputProps) : JSX.Element {
+  return (
+    <FormControl mt={3}>
+      {label && <FormControl.Label>{label}</FormControl.Label>}
+      <Input
+        placeholder={placeholder}
+        size="lg"
+        w="100%"
+        borderRadius="lg"
+        bgColor="gray.100"
+        secureTextEntry={secureTextEntry}
+        shadow={3}
+      />
+    </FormControl>
+  );
+};
---
+++ src/app/components/Title.tsx
@@ -0,0 +1,22 @@
+import { Text, ITextProps } from "native-base"
+import { ReactNode } from "react"
+
+interface TitleProps extends ITextProps {
+    children: ReactNode
+}
+
+export function Title({ children, ...rest }: TitleProps) {
+    return (
+      <Text
+        fontSize="2xl"
+        fontWeight="bold"
+        color="gray.500"
+        textAlign="center"
+        marginTop={5}
+        {...rest}
+      >
+        {children}
+      </Text>
+    );
+  }
+  
