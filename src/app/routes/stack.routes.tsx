01436dc68cb0c7260cb0c3eea5dffa35665e7e40
jotaapee <galhardojoaopedro@gmail.com>
2024-03-05 21:09:57 -0300

    changing and creating files

--- src/app/routes/stack.routes.tsx
+++ src/app/routes/stack.routes.tsx
@@ -1,12 +1,27 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Login } from "../screens/Login";
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Register from "../screens/Register"
+import Login  from "../screens/Login";
 
 const Stack = createNativeStackNavigator();
 
-export default function StackRoutes() {
+export default function Routes() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Login} />
-    </Stack.Navigator>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Login'
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='Register'
+          component={Register}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
---
+++ src/styles/themes.ts
@@ -0,0 +1,22 @@
+import { extendTheme } from 'native-base'
+
+export const THEMES = extendTheme({
+    colors: {
+        gray: {
+            300: '#8D8D99'
+        },
+        green: {
+            500: '#127856',
+            800: '#739489'
+        },
+        white: '#fff',
+        black: '#000'
+    },
+    fontSizes: {
+        xs: 12,
+        sm: 14,
+        md: 16,
+        lg: 20,
+        xl: 24        
+    } 
+}) 
