40d2a00ab816b40c239dcd08795401c62c8076fb
jotaapee <galhardojoaopedro@gmail.com>
2024-03-14 11:39:24 -0300

    husky-prettier-eslint config

--- App.tsx
+++ App.tsx
@@ -2,18 +2,21 @@ import { NativeBaseProvider, StatusBar } from "native-base";
 import Routes from "@routes/stack.routes";
 import theme from "./src/app/theme";
 import { ThemeProvider } from "styled-components";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
     return (
-        <NativeBaseProvider>
-            <ThemeProvider theme={theme}>
-                <StatusBar
-                    barStyle="light-content"
-                    backgroundColor="transparent"
-                    translucent
-                />
-                <Routes />
-            </ThemeProvider>
-        </NativeBaseProvider>
+        <SafeAreaView style={{flex: 1}}>
+            <NativeBaseProvider>
+                <ThemeProvider theme={theme}>
+                    <StatusBar
+                        barStyle="light-content"
+                        backgroundColor="transparent"
+                        translucent
+                    />
+                    <Routes />
+                </ThemeProvider>
+            </NativeBaseProvider>
+        </SafeAreaView>
     );
 }
--- src/app/theme/index.ts
+++ src/app/theme/index.ts
@@ -1,27 +1,30 @@
 export default {
     COLORS: {
-        WHITE: "#FFFFFF",
+      WHITE: '#FFFFFF',
+  
+      GREEN_700: '#739489',
+      GREEN_500: '#127856',
+      GREEN_300: '#459B7E',
 
-        GREEN_700: "#739489",
-        GREEN_500: "#127856",
-        GREEN_300: "#459B7E",
+      RED_100: '#AD2A2A',
+  
+      GRAY_700: '#000000',
+      GRAY_600: '#919191',
+      GRAY_500: '#8D8D99',
+      GRAY_400: '#BABABA',
+      GRAY_300: '#D9D9D9',
 
-        RED_100: "#BC5252",
+      BLUE_100: '#459EB1'
 
-        GRAY_700: "#000000",
-        GRAY_600: "#919191",
-        GRAY_500: "#8D8D99",
-        GRAY_400: "#BABABA",
-        GRAY_300: "#D9D9D9"
     },
     FONT_FAMILY: {
-        REGULAR: "Roboto_400Regular",
-        BOLD: "Roboto_700Bold"
+      REGULAR: 'Jost_400Regular',
+      BOLD: 'Jost_700Bold'
     },
     FONT_SIZE: {
-        SM: 14,
-        MD: 16,
-        LG: 18,
-        XL: 24
+      SM: '14px',
+      MD: '16px',
+      LG: '18px',
+      XL: '24px'
     }
-};
+  };
