3182926adcee044dec936c154bcb2b5a84da6895
jotaapee <galhardojoaopedro@gmail.com>
2024-03-18 15:15:40 -0300

    alterating routes

--- src/app/routes/stack.routes.tsx
+++ src/app/routes/stack.routes.tsx
@@ -1,6 +1,6 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import DrawerRoutes from "@routes/drawer.routes";
 import ConsultationsHeld from "@screens/ConsultationsHeld";
 import DetailsConsultationsHeld from "@screens/DetailsConsultationsHeld";
 import ExamsDetected from "@screens/ExamsDetected";
@@ -9,13 +9,10 @@ import ExamsHistory from "@screens/ExamsHistory";
 import Glucose from "@screens/Glucose";
 import IMC from "@screens/IMC";
 import ListConsultationsHeld from "@screens/ListConsultationsHeld";
-import Main from "@screens/Main";
 import Medicines from "@screens/Medicines";
 import NewMedicine from "@screens/NewMedicine";
 import Perfil from "@screens/Perfil";
 import Pressure from "@screens/Pressure";
-import { Image } from "native-base";
-import { TouchableOpacity } from "react-native-gesture-handler";
 
 import About from "../screens/About";
 import Home from "../screens/Home";
@@ -23,109 +20,6 @@ import Login from "../screens/Login";
 import Register from "../screens/Register";
 
 const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
-
-function DrawerRoutes({ navigation }: any) {
-    return (
-        <Drawer.Navigator
-            screenOptions={{
-                drawerStyle: {
-                    backgroundColor: "#FFFFFF"
-                },
-                drawerLabelStyle: {
-                    color: "#000000",
-                    fontSize: 16,
-                    fontFamily: "Jost_400Regular",
-                    fontWeight: "400",
-                    lineHeight: 20
-                },
-                headerTintColor: "white",
-                headerRight: () => (
-                    <TouchableOpacity
-                        style={{ display: "flex", gap: 10 }}
-                        onPress={() => navigation.navigate("Perfil")}
-                    >
-                        <Image
-                            source={require("../../../assets/perfill.png")}
-                            alt="Perfil"
-                            style={{ width: 30, height: 30, marginRight: 15 }}
-                        />
-                    </TouchableOpacity>
-                )
-            }}
-        >
-            <Drawer.Screen
-                name="YE GESTÃO SAÚDE"
-                component={Main}
-                options={{
-                    drawerLabel: "YE GESTÃO DE SAÚDE",
-                    drawerIcon: () => (
-                        <Image
-                            source={require("../../../assets/logom.png")}
-                            alt="Logo"
-                            style={{ width: 24, height: 24 }}
-                        />
-                    ),
-                    headerStyle: {
-                        backgroundColor: "#739489"
-                    },
-                    title: ""
-                }}
-            />
-            <Drawer.Screen
-                name="Exames"
-                component={ExamsHistory}
-                options={{
-                    drawerLabel: "Exames",
-                    drawerIcon: () => (
-                        <Image
-                            source={require("../../../assets/exames.png")}
-                            alt="Exams"
-                            style={{ width: 24, height: 24 }}
-                        />
-                    ),
-                    headerTransparent: true,
-                    title: "",
-                    headerShown: false
-                }}
-            />
-            <Drawer.Screen
-                name="Consultas"
-                component={ConsultationsHeld}
-                options={{
-                    drawerLabel: "Consultas",
-                    drawerIcon: () => (
-                        <Image
-                            source={require("../../../assets/consultas.png")}
-                            alt="Consultas"
-                            style={{ width: 24, height: 24 }}
-                        />
-                    ),
-                    headerTransparent: true,
-                    title: "",
-                    headerShown: false
-                }}
-            />
-            <Drawer.Screen
-                name="Medicamentos"
-                component={Medicines}
-                options={{
-                    drawerLabel: "Medicamentos",
-                    drawerIcon: () => (
-                        <Image
-                            source={require("../../../assets/medicamentos.png")}
-                            alt="Medicamentos"
-                            style={{ width: 24, height: 24 }}
-                        />
-                    ),
-                    headerTransparent: true,
-                    title: "",
-                    headerShown: false
-                }}
-            />
-        </Drawer.Navigator>
-    );
-}
 
 export default function Routes() {
     return (
