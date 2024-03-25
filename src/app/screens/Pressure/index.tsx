4ed6b71e6864a368025c81439770aebe8af57c4c
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-03-25 07:28:26 -0300

    pressure with pop up

--- src/app/screens/Pressure/index.tsx
+++ src/app/screens/Pressure/index.tsx
@@ -1,60 +1,81 @@
 import { Header } from "@components/Header";
+import { PopUpAddButton } from "@components/PopUpAddButton";
 import { Table } from "@components/Table";
 import { FlatList } from "native-base";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import PopUp from "src/app/components/PopUp";
 
-// const json = [
-//     {
-//         "data": "23/08/2024",
-//         "medida": "12,3",
-//         "nivel": "4"
-//     },
-//     {
-//         "data": "23/08/2024",
-//         "medida": "12,3",
-//         "nivel": "4"
-//     },
-//     {
-//         "data": "23/08/2024",
-//         "medida": "12,3",
-//         "nivel": "4"
-//     },
-//     {
-//         "data": "23/08/2024",
-//         "medida": "12,3",
-//         "nivel": "4"
-//     },
-//     {
-//         "data": "23/08/2024",
-//         "medida": "12,3",
-//         "nivel": "4"
-//     },
-//     {
-//         "data": "23/08/2024",
-//         "medida": "12,3",
-//         "nivel": "4"
-//     },
-// ]
+const json = [
+    {
+        id: 1,
+        data: "Data",
+        medida: "Medida",
+        nivel: "Nível"
+    },
+    {
+        id: 2,
+        data: "05/04/2023",
+        medida: "120 x 80",
+        nivel: "Normal"
+    },
+    {
+        id: 3,
+        data: "06/04/2023",
+        medida: "110 x 70",
+        nivel: "Normal"
+    },
+    {
+        id: 4,
+        data: "07/04/2023",
+        medida: "150 x 100",
+        nivel: "Alta"
+    }
+];
 
 export default function Pressure() {
-    // const [data, setData] = useState([{}])
-    // setData(json)
+    const [data, setData] = useState([{}]);
+    const [showPopUp, setShowPopUp] = useState(false);
 
-    const [rows, setRows] = useState<string[][]>([
-        ["Data", "Medida", "Nível"],
-        ["05/04/2023", "120 x 80", "Normal"],
-        ["06/04/2023", "110 x 70", "Normal"],
-        ["07/04/2023", "150 x 100", "Alta"]
-    ]);
+    const handleOpenPopUp = () => {
+        setShowPopUp(true);
+    };
+
+    const handleClosePopUp = () => {
+        setShowPopUp(false);
+    };
+
+    const handlePost = (date: string, measure: string) => {
+        json.push({
+            id: json.findLastIndex((value) => value.id) + 1,
+            data: date,
+            medida: measure,
+            nivel: "Normal"
+        });
+    };
+
+    json.findLastIndex((value) => value.id);
+
+    useEffect(() => {
+        setData(json);
+    }, []);
 
     return (
         <>
             <Header text="Pressão Arterial" isBackPress />
+
             <FlatList
-                data={rows}
-                keyExtractor={(_, index) => index.toString()}
+                data={data}
+                keyExtractor={(item) => item.id}
                 renderItem={({ item }) => <Table rows={[item]} />}
             />
+
+            <PopUpAddButton onOpen={handleOpenPopUp} />
+
+            {showPopUp ? (
+                <PopUp onClose={handleClosePopUp} onPost={handlePost} />
+            ) : (
+                <></>
+            )}
         </>
     );
 }
