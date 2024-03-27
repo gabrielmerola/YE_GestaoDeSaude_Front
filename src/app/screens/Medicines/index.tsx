5b6636c6c6e65683312b0e06620d0450aee12c6d
ZettDev <23.00171-2@maua.br>
2024-03-27 12:32:51 -0300

    medicines modal refactoring started

--- src/app/screens/Medicines/index.tsx
+++ src/app/screens/Medicines/index.tsx
@@ -1,37 +1,84 @@
 import BottomAddButton from "@components/BottomAddButton";
+import CancelAndSaveButton from "@components/CancelAndSaveButton";
 import { Header } from "@components/Header";
 import MedicinesAndButton from "@components/MedicinesAndButton";
-import { View } from "@screens/Medicines/styles";
-import React from "react";
+import { PopUpAddButton } from "@components/PopUpAddButton";
+import {
+    ButtonContainer,
+    ModalContainer,
+    View
+} from "@screens/Medicines/styles";
+import React, { useState } from "react";
+
+const json = [
+    {
+        text: "Losartana",
+        function: () => console.log("Losartana")
+    }
+];
 
 export default function Medicines({ navigation }: any) {
+    const [showNewMedicines, setShowNewMedicines] = useState(true);
+
     return (
         <>
             <Header text="Medicamentos" isBackPress />
             <View>
-                <MedicinesAndButton
-                    text="Losartana"
-                    text2="08:00"
-                    chevron
-                    navigation={navigation}
-                    screenName="Login"
-                />
-                <MedicinesAndButton
-                    text="Hidrocolorotiazida"
-                    text2="08:00"
-                    chevron
-                    navigation={navigation}
-                    screenName="Login"
-                />
-                <MedicinesAndButton
-                    text="Pantoprazol"
-                    text2="12:00"
-                    chevron
-                    navigation={navigation}
-                    screenName="Login"
-                />
+                {/*<MedicinesAndButton*/}
+                {/*    text="Losartana"*/}
+                {/*    text2="08:00"*/}
+                {/*    chevron*/}
+                {/*/>*/}
+                {/*<MedicinesAndButton*/}
+                {/*    text="Hidrocolorotiazida"*/}
+                {/*    text2="08:00"*/}
+                {/*    chevron*/}
+                {/*/>*/}
+                {/*<MedicinesAndButton*/}
+                {/*    text="Pantoprazol"*/}
+                {/*    text2="12:00"*/}
+                {/*    chevron*/}
+                {/*/>*/}
             </View>
-            <BottomAddButton screenName="NewMedicine" navigation={navigation} />
+            <ButtonContainer>
+                <PopUpAddButton onOpen={() => setShowNewMedicines(true)} />
+            </ButtonContainer>
+            {/*<BottomAddButton screenName="NewMedicine" navigation={navigation} />*/}
+
+            {showNewMedicines ? (
+                <ModalContainer>
+                    <Header
+                        text="Novo Medicamento"
+                        isModal
+                        onModalClose={() => setShowNewMedicines(false)}
+                    />
+                    {/*<View>*/}
+                    {/*    <MedicinesAndButton*/}
+                    {/*        text="Nome: "*/}
+                    {/*        input*/}
+                    {/*    />*/}
+                    {/*    <MedicinesAndButton*/}
+                    {/*        text="Horário"*/}
+                    {/*        text2="09:00"*/}
+                    {/*    />*/}
+                    {/*    <MedicinesAndButton*/}
+                    {/*        text="Período"*/}
+                    {/*        text2="7 dias"*/}
+                    {/*    />*/}
+                    {/*    <MedicinesAndButton*/}
+                    {/*        text="Intervalo"*/}
+                    {/*        text2="8hs"*/}
+                    {/*    />*/}
+                    {/*    <MedicinesAndButton*/}
+                    {/*        text="Quantidade"*/}
+                    {/*        text2="1"*/}
+                    {/*    />*/}
+                    {/*</View>*/}
+                    {/*<CancelAndSaveButton/>*/}
+                </ModalContainer>
+            ) : (
+                <></>
+            )}
         </>
     );
 }
