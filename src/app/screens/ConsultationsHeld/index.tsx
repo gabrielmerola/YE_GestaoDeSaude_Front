059858396b24fbdb0df3512582b7b79d9f9b3a93
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-04-01 23:19:17 -0300

    consultations modal started

--- src/app/screens/ConsultationsHeld/index.tsx
+++ src/app/screens/ConsultationsHeld/index.tsx
@@ -1,30 +1,60 @@
 import BottomAddButton from "@components/BottomAddButton";
+import CancelAndSaveButton from "@components/CancelAndSaveButton";
 import { Header } from "@components/Header";
-import MedicinesAndButton from "@components/MedicinesAndButton";
-import { View } from "@screens/ConsultationsHeld/styles";
+import { PopUpAddButton } from "@components/PopUpAddButton";
+import {
+    ButtonContainer,
+    ModalContainer,
+    View
+} from "@screens/ConsultationsHeld/styles";
 import React, { useState } from "react";
+import ListInteractableItem from "src/app/components/ListInteractableItem";
 
 export default function ConsultationsHeld({ navigation }: any) {
-    const [showNewMedicines, setShowNewMedicines] = useState(true);
+    const [showConsultationsData, setShowConsultationsData] = useState(false);
 
     return (
         <>
             <Header text="Consultas Realizadas" isBackPress />
             <View>
-                <MedicinesAndButton
+                <ListInteractableItem
                     text="Clínico Geral"
-                    chevron
-                    modalFunction={() => setShowNewMedicines(true)}
+                    isButton
+                    modalFunction={() => setShowConsultationsData(true)}
                 />
-                {/*<MedicinesAndButton*/}
-                {/*    text="Ginecologista"*/}
-                {/*    chevron*/}
-                {/*/>*/}
-                <BottomAddButton
-                    screenName="NewMedicine"
-                    navigation={navigation}
-                />
+                <ListInteractableItem text="Ginecologista" isButton />
+                <ButtonContainer>
+                    <PopUpAddButton
+                        onOpen={() => setShowConsultationsData(true)}
+                    />
+                </ButtonContainer>
             </View>
+
+            {showConsultationsData ? (
+                <ModalContainer>
+                    <Header
+                        text="Consultas Realizadas"
+                        isModal
+                        onModalClose={() => setShowConsultationsData(false)}
+                    />
+                    <View>
+                        <ListInteractableItem
+                            text="Próxima consulta"
+                            text2="17/08/2023"
+                            isButton
+                        />
+                        <ListInteractableItem text="17/02/2023" isButton />
+                        <ListInteractableItem text="25/05/2022" isButton />
+                        <ListInteractableItem text="04/01/2022" isButton />
+                        <ListInteractableItem text="30/09/2020" isButton />
+                    </View>
+                    <CancelAndSaveButton
+                        onPress={() => setShowConsultationsData(false)}
+                    />
+                </ModalContainer>
+            ) : (
+                <></>
+            )}
         </>
     );
 }
--- src/app/screens/ListConsultationsHeld/index.tsx
+++ src/app/screens/ListConsultationsHeld/index.tsx
@@ -1,38 +1,20 @@
 import { Header } from "@components/Header";
-import MedicinesAndButton from "@components/MedicinesAndButton";
 import { View } from "@screens/ListConsultationsHeld/styles";
 import React from "react";
+import ListInteractableItem from "src/app/components/ListInteractableItem";
 
 export default function ListConsultationsHeld({ navigation }: any) {
     return (
         <>
             <Header text="Consultas Realizadas" isBackPress />
             <View>
-                <MedicinesAndButton
+                <ListInteractableItem
                     text="Próxima consulta"
                     text2="17/08/2023"
-                    chevron
-                    navigation={navigation}
-                    screenName="Login"
                 />
-                <MedicinesAndButton
-                    text="17/02/2023"
-                    chevron
-                    navigation={navigation}
-                    screenName="Login"
-                />
-                <MedicinesAndButton
-                    text="25/05/2022"
-                    chevron
-                    navigation={navigation}
-                    screenName="Login"
-                />
-                <MedicinesAndButton
-                    text="04/01/2022"
-                    chevron
-                    navigation={navigation}
-                    screenName="Login"
-                />
+                <ListInteractableItem text="17/02/2023" />
+                <ListInteractableItem text="25/05/2022" />
+                <ListInteractableItem text="04/01/2022" />
             </View>
         </>
     );
