d809994944fe1066edc5a2f1042821034b407029
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-05-01 22:41:56 -0300

    Glucose Requests finished

--- src/app/screens/Glucose/index.tsx
+++ src/app/screens/Glucose/index.tsx
@@ -9,7 +9,7 @@ import PopUp from "src/app/components/PopUp";
 interface Glucose {
     [key: string]: string | number;
     id: number;
-    data: string;
+    date: string;
     measure: string;
     level: string;
 }
@@ -28,10 +28,37 @@ export default function Glucose() {
         setShowPopUp(false);
     };
 
+    function loadData() {
+        const fetchData = async () => {
+            try {
+                const glucoseData = await getGlucose();
+
+                const sortedData = glucoseData.sort(
+                    (a: Glucose, b: Glucose) => {
+                        const dateA = new Date(a.date + "T00:00:01");
+                        const dateB = new Date(b.date + "T00:00:01");
+                        return dateA - dateB;
+                    }
+                );
+
+                const formattedData = sortedData.map((item) => ({
+                    ...item,
+                    date: formatDate(item.date)
+                }));
+
+                setData(formattedData);
+            } catch (error) {
+                console.error("Error fetching glucose data:", error);
+            }
+        };
+        fetchData();
+    }
+
     const handlePost = (list: object) => {
         const response = postGlucose(list);
         response.then((json) => {
             console.log(json);
+            loadData();
         });
     };
 
@@ -39,14 +66,20 @@ export default function Glucose() {
         const response = deleteGlucose(id);
         response.then((json) => {
             console.log(json);
+            loadData();
         });
     };
 
+    const formatDate = (dateString: Date) => {
+        const date = new Date(dateString + "T00:00:01");
+        const day = date.getDate().toString().padStart(2, "0"); // Get day with leading zero if needed
+        const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Get month with leading zero if needed
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    };
+
     useEffect(() => {
-        const data = getGlucose();
-        data.then((data) => {
-            setData(data);
-        });
+        loadData();
     }, []);
 
     return (
@@ -65,6 +98,7 @@ export default function Glucose() {
                 <PopUp
                     onClose={handleClosePopUp}
                     onPost={handlePost}
+                    onUpdate={loadData}
                     popUpType="GLUCOSE"
                 />
             ) : (
