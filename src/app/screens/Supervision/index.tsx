13cfda1170b965087286deeea9c4644b13a1564f
Breno1101 <brenocoutinhorodrigues@gmail.com>
2024-06-11 11:05:56 -0300

    Adding toast to verify email

--- src/app/screens/Supervision/index.tsx
+++ src/app/screens/Supervision/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@components/Header";
 import { VStack, ScrollView, View, Input } from "native-base";
-import Icons from "react-native-vector-icons/FontAwesome6";
+import { useToast } from "native-base";
+import { useState } from "react";
 
 import {
     TextTitle,
@@ -12,21 +13,46 @@ import {
 } from "./styles";
 
 export default function Supervision() {
+    const [email, setEmail] = useState<string>('');
+    const toast = useToast();
+
+    const validateEmail = (email: string) => {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    };
+
+    const handlePress = () => {
+        if (validateEmail(email)) {
+            toast.show({
+                title: "Concluído!",
+                description: "Email válidado com sucesso.",
+                backgroundColor: "green.500",
+                placement: "top"
+            });
+            setEmail('');
+        } else {
+            toast.show({
+                title: "Email inválido!",
+                description: "Preencha novamente o email.",
+                backgroundColor: "red.500",
+                placement: "top"
+            });
+        }
+    };
+
     return (
         <ScrollView flex={1}>
             <VStack flex={1} alignItems="center">
                 <Header text="Supervisão" isBackPress />
                 <View>
                     <TextTitle>
-                        Criado para auxilio de notificações com familiares e
-                        responsáveis.
+                        Criado para auxilio de notificações com familiares e responsáveis.
                     </TextTitle>
                 </View>
 
                 <ViewCard>
                     <TextSubtitleWhite>
-                        Insira o email de quem irá ajuda-lo com as notificações
-                        do aplicativo:
+                        Insira o email de quem irá ajuda-lo com as notificações do aplicativo:
                     </TextSubtitleWhite>
 
                     <Input
@@ -41,20 +67,12 @@ export default function Supervision() {
                         alignSelf="center"
                         editable
                         maxLength={40}
+                        value={email}  
+                        onChangeText={setEmail}  
                     />
 
-                    <TextNotificationWhite>
-                        <Icons name="check" size={18} />
-                        ㅤEmail válido e salvo para análise...
-                    </TextNotificationWhite>
-
-                    <TextNotificationWhite>
-                        <Icons name="xmark" size={20} />
-                        ㅤEmail inválido! Tente novamente...
-                    </TextNotificationWhite>
-
                     <View alignItems="center">
-                        <Button>
+                        <Button onPress={handlePress}>
                             <ButtonTextWhite>Confirmar</ButtonTextWhite>
                         </Button>
                     </View>
